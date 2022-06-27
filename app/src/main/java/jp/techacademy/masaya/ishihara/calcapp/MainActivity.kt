e9979eca10e7ae365793c4bb3d68ac4c38b2dbcc
BackPonBeauty <vf2_casino@yahoo.co.jp>
2022-06-27 19:23:25 +0900

    Initial Commit

--- app/src/main/java/jp/techacademy/masaya/ishihara/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/masaya/ishihara/calcapp/MainActivity.kt
@@ -10,7 +10,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.text.Editable
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import java.text.DecimalFormat
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -38,20 +40,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }else if(v.id == R.id.button4){
                 number3 = "/"
             }
-            Log.d("LogD", number1 + ":" + number2.length.toString() + ":" + number3)
+            Log.d("LogD", number1 + ":" + number2 + ":" + number3)
             if (number1.length != 0 && number2.length != 0) {
-                if(number1 != "." && number2 != "."){
-                    intent.putExtra("VALUE1", number1)
-                    intent.putExtra("VALUE2", number2)
-                    intent.putExtra("VALUE3", number3)
-                    startActivity(intent)
+                try {
+                    var num_F1: String = "${number1.toDouble() / number1.toDouble()}"
+                    var num_F2: String = "${number2.toDouble() / number2.toDouble()}"
+                    Log.d("LogD", num_F1.toString() + num_F2.toString())
+                    if(num_F1.toString() == "1.0" && num_F2.toString() == "1.0"){
+                        intent.putExtra("VALUE1", number1)
+                        intent.putExtra("VALUE2", number2)
+                        intent.putExtra("VALUE3", number3)
+                        startActivity(intent)
+                    }
+                }catch(e:NumberFormatException){
+                    showAlertDialog()
                 }
-
             }
         }
+    }
+    private fun showAlertDialog() {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("エラー")
+        alertDialogBuilder.setMessage("正しい値を入力してください")
 
+        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("OK"){dialog, which ->
+            Log.d("UI_PARTS", "OK")
+        }
+
+
+
+        // AlertDialogを作成して表示する
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
+
 
 
 
